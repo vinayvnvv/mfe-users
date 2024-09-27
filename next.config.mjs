09be8f8fe50f72b1f0f20dc33a49a24e93ada2a3
vinaybv <vinay.bv@accionlabs.com>
2024-09-27 17:03:33 +0530

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
+// remote/next.config.js
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
+
 const nextConfig = {
   reactStrictMode: true,
-};
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "users",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./users": "./src/pages/users/index.js",
+        },
+        shared: {
+          // specify shared dependencies
+          // read more in Shared Dependencies section
+        },
+        extraOptions: {
+          exposePages: true,
+        },
+      })
+    );
 
+    return config;
+  },
+};
 export default nextConfig;
